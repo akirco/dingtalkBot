2cfa2f2abd3036463754740e1cdfc815c99c7f0d
Casuor <1833708553@qq.com>
2022-07-15 12:27:22 +0800

    upload

--- src/components/UploadFile.vue
+++ src/components/UploadFile.vue
@@ -1,28 +1,56 @@
 <script setup lang="ts">
-import DialogPanel from "./DialogPanel.vue";
-import { useModalStore } from "@/stores/modal";
-import {imgToBase64} from "@/utils/imgtobase64"
+import DialogPanel from './DialogPanel.vue';
+import { useModalStore } from '@/stores/modal';
+import { ref, type Ref } from 'vue';
+import service from '@/api/http';
+
 const modal = useModalStore();
 const openModal = modal.openModal;
+
+const imgsrc1 = ref();
+const imgsrc2 = ref();
+const msg = ref();
+const createImgPreview = (file: Blob) => {
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onload = (e) => {
+    imgsrc1.value = e.target?.result;
+    imgsrc2.value = e.target?.result;
+  };
+};
+//preview image with imgsrc and upload to server
+
+const uploadImage = (e: Event) => {
+  const currentTarget = e.target as HTMLInputElement;
+  if (currentTarget.files) {
+    const files = currentTarget.files;
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      console.log(file);
+
+      createImgPreview(file);
+    }
+  }
+};
+
+const data = { img1: imgsrc1, img2: imgsrc2, msg: msg };
+console.log(data);
+
 const onSubmit = (e: Event) => {
   e.preventDefault();
-  console.log("submit");
-  openModal();
+  service.post('/api/v1/insert', data).then((res) => {
+    // openModal();
+    console.log(res);
+  });
 };
-const changeImage=(e:any)=>{
-  let img = e.target.files[0];
-  imgToBase64(img).then((res: any)=>{
-    console.log(res);  
-  })
-}
 </script>
 
 <template>
   <DialogPanel />
   <div>
-    <div class="mt-5 md:mt-0 md:col-span-2 ">
-      <form action="#" method="POST">
-        <div class="shadow sm:rounded-md sm:overflow-hidden ">
+    <div class="mt-5 md:mt-0 md:col-span-2">
+      <form action="#" method="POST" enctype="multipart/form-data">
+        <div class="shadow sm:rounded-md sm:overflow-hidden">
           <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
             <div>
               <label class="block text-sm font-medium text-gray-700">
@@ -38,6 +66,7 @@ const changeImage=(e:any)=>{
                     fill="none"
                     viewBox="0 0 48 48"
                     aria-hidden="true"
+                    v-if="!imgsrc1"
                   >
                     <path
                       d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
@@ -46,46 +75,21 @@ const changeImage=(e:any)=>{
                       stroke-linejoin="round"
                     />
                   </svg>
-                  <img :src="imgsrc" alt="">
-                  <div class="flex text-sm text-gray-600">
-                    <label
-                      for="file-upload"
-                      class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-                    >
-                      <span>Upload a file</span>
-                      <input
-                        id="file-upload"
-                        name="file-upload"
-                        type="file"
-                        class="sr-only"
-                        accept="image/gif,image/jpeg,image/jpg,image/png"
-                        @change="changeImage($event)"
-                      />
-                    </label>
-                    <p class="pl-1">or drag and drop</p>
+                  <div class="flex">
+                    <img
+                      v-if="imgsrc1"
+                      :src="imgsrc1"
+                      alt="preview"
+                      style="width: 200px; height: 200px; display: block"
+                    />
+                    <img
+                      v-if="imgsrc2"
+                      :src="imgsrc2"
+                      alt="preview"
+                      style="width: 200px; height: 200px; display: block"
+                    />
                   </div>
-                  <p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
-                </div>
-              </div>
-                <div
-                class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md"
-              >
-                <div class="space-y-1 text-center">
-                  <svg
-                    class="mx-auto h-12 w-12 text-gray-400"
-                    stroke="currentColor"
-                    fill="none"
-                    viewBox="0 0 48 48"
-                    aria-hidden="true"
-                  >
-                    <path
-                      d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                  </svg>
-                  <div class="flex text-sm text-gray-600">
+                  <div class="text-sm text-gray-600">
                     <label
                       for="file-upload"
                       class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
@@ -96,10 +100,11 @@ const changeImage=(e:any)=>{
                         name="file-upload"
                         type="file"
                         class="sr-only"
-                        accept="image/gif,image/jpeg,image/jpg,image/png"
+                        accept="image/*"
+                        multiple
+                        @change="uploadImage($event)"
                       />
                     </label>
-                    <p class="pl-1">or drag and drop</p>
                   </div>
                   <p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
                 </div>
@@ -114,6 +119,7 @@ const changeImage=(e:any)=>{
                   id="msg"
                   name="msg"
                   rows="3"
+                  :value="msg"
                   class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
                 />
               </div>
---
+++ src/utils/base64ToBlob.ts
@@ -0,0 +1,10 @@
+﻿export function base64ToBlob(base64: string, type: string) {
+  const bytes = window.atob(base64);
+  const len = bytes.length;
+  const buffer = new ArrayBuffer(len);
+  const view = new Uint8Array(buffer);
+  for (let i = 0; i < len; i++) {
+    view[i] = bytes.charCodeAt(i);
+  }
+  return new Blob([buffer], { type });
+}
