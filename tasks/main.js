89f9d350ffb8395fb75cf138df61e5c3c2f51454
akirco <1833708553@qq.com>
2022-07-19 14:56:07 +0800

    lit:test

--- tasks/main.js
+++ tasks/main.js
@@ -1,15 +1,19 @@
 ﻿const { excuteSqlConn } = require('../utils/sql');
 const ChatBot = require('dingtalk-robot-sender');
 
+
+
 async function getBots(callback) {
   const bots = await excuteSqlConn(`SELECT * FROM bot`);
   callback(bots);
 }
 
 function setDingTalkBot(bots) {
+  const storage = []
   bots.map((bot) => {
     bot = JSON.parse(JSON.stringify(bot));
-    const robot = new ChatBot(bot);
+    storage.push(bot);
+    // const robot = new ChatBot(bot);
   });
 }
 
