d2975235d271791d46990ce55cf79639bfd39640
akicro <1833708553@qq.com>
2022-07-29 16:19:13 +0800

    change: insert jobs

--- src/service/jobs.service.js
+++ src/service/jobs.service.js
@@ -1,19 +1,19 @@
-﻿const { excuteSql } = require('../utils/sql');
+﻿const { excuteSql } = require("../utils/sql");
 const jsonFormate = require("../utils/jsonFormate");
 class jobService {
   async jobSelectByBotId(ctx, next) {
     try {
-      let _sql =  `select * from jobs where botId = ?`;
-      let _values = [ctx.request.query.botId]
-      const result = await excuteSql(_sql,_values);
+      let _sql = `select * from jobs where botId = ?`;
+      let _values = [ctx.request.query.botId];
+      const result = await excuteSql(_sql, _values);
       ctx.body = {
-        msg: 'select success',
+        msg: "select success",
         data: result,
         code: 200,
       };
     } catch (error) {
       ctx.body = {
-        msg: 'select error',
+        msg: "select error",
         data: error.sqlMessage || null,
         code: 500,
       };
@@ -21,8 +21,11 @@ class jobService {
   }
   async jobInsert(ctx, next) {
     try {
-      let _sql = 'insert into jobs(botId, img1, img2, img3, txt1, txt2) values (?,?,?,?,?,?)';
+      let _sql =
+        "insert into jobs(uid, remark,img1, img2, img3, txt1, txt2) values (?,?,?,?,?,?,?);";
       let _values = [
+        ctx.request.body.uid,
+        ctx.request.body.remark,
         ctx.request.body.img1,
         ctx.request.body.img2,
         ctx.request.body.img3,
@@ -31,13 +34,13 @@ class jobService {
       ];
       const result = await excuteSql(_sql, _values);
       ctx.body = {
-        msg: 'insert success',
+        msg: "insert success",
         data: result.id,
         code: 200,
       };
     } catch (error) {
       ctx.body = {
-        msg: 'insert error',
+        msg: "insert error",
         data: error.sqlMessage || null,
         code: 500,
       };
@@ -45,24 +48,25 @@ class jobService {
   }
   async jobUpdateByBotId(ctx, next) {
     try {
-      let _sql = 'update jobs set img1=?, img2=?,img3=?,txt1=?,txt2=? WHERE botId=?';
+      let _sql =
+        "update jobs set img1=?, img2=?,img3=?,txt1=?,txt2=? WHERE botId=?";
       let _values = [
         ctx.request.body.img1,
         ctx.request.body.img2,
         ctx.request.body.img3,
         ctx.request.body.txt1,
         ctx.request.body.txt2,
-        ctx.request.body.botId
+        ctx.request.body.botId,
       ];
       const result = await excuteSql(_sql, _values);
       ctx.body = {
-        msg: 'update success',
+        msg: "update success",
         data: result.id,
         code: 200,
       };
     } catch (error) {
       ctx.body = {
-        msg: 'update error',
+        msg: "update error",
         data: error.sqlMessage || null,
         code: 500,
       };
@@ -70,18 +74,18 @@ class jobService {
   }
   async jobDeleteByBotId(ctx, next) {
     try {
-      let _sql = 'delete from jobs where botId=?';
+      let _sql = "delete from jobs where botId=?";
       let _values = [ctx.params.botId];
       let result = await excuteSql(_sql, _values);
       result = jsonFormate(result);
       ctx.body = {
-        msg: 'del success',
+        msg: "del success",
         data: `影响行数为:${result}条`,
         code: 200,
       };
     } catch (error) {
       ctx.body = {
-        msg: 'del error',
+        msg: "del error",
         data: error.sqlMessage || null,
         code: 500,
       };
