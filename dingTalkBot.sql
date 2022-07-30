5d75045df9bedc4e717d1cb7a6a7a65bc31fb4d6
akicro <1833708553@qq.com>
2022-07-30 18:12:13 +0800

    feat: serve static file

--- dingTalkBot.sql
+++ dingTalkBot.sql
@@ -21,26 +21,28 @@ create table if not exists bot
 (
     botId       int          not null auto_increment primary key comment '机器人ID',
     uid         int          not null comment '用户ID',
-    baseUrl     varchar(255) not null default 'https://oapi.dingtalk.com/robot/send',
-    accessToken varchar(255) not null comment 'webhook地址',
+    baseUrl     varchar(255) not null default 'https://oapi.dingtalk.com/robot/send' comment 'webhook baseUrl',
+    accessToken varchar(255) not null comment 'webhook accessToken',
     secret      varchar(255) not null comment '机器人密钥'
 ) engine = InnoDB
   DEFAULT CHARSET = utf8;
-
-
-
-insert into bot(uid,accessToken, secret)
-values (2, '96795b7d4da817831fecacbb3cb9b41ca4135501342bf5e3abf68558f5fabc1f','SEC08b40c1a4cffded730e594eabd5ef513c79a07fad104301c0ca7e1fc9a5961e3');
 
 insert into bot(uid,accessToken, secret)
 values (1, 'd49fd2eb77d0a96520a3ec6261b514f02cee6e61b06d9a49723338696683777e','SECeb8f227ee9c919d68d171cf2a3cb1949b26a971616dcbd4e687ca56317e96066');
 
 
 
+insert into bot(uid,accessToken, secret)
+values (2, '96795b7d4da817831fecacbb3cb9b41ca4135501342bf5e3abf68558f5fabc1f','SEC08b40c1a4cffded730e594eabd5ef513c79a07fad104301c0ca7e1fc9a5961e3');
+
+
+
 create table if not exists jobs
 (
     id       int          not null auto_increment primary key comment 'job id',
-    botId    int          not null comment '机器人id',
+    uid      int          not null comment '用户id',
+    botId    int          comment '机器人id',
+    remark   varchar(255) not null comment '备注',
     img1     varchar(255) not null comment '图1',
     img2     varchar(255) not null comment '图2',
     img3     varchar(255) comment '图3',
@@ -52,20 +54,54 @@ create table if not exists jobs
 ) engine = InnoDB
   DEFAULT CHARSET = utf8;
 
+insert into jobs(uid, remark,img1, img2, img3, txt1, txt2)
+values (1,'测试群1',
+        'image-1659081780631.png',
+        'image-1659081780633.png',
+        '',
+        '测试数据1', '测试数据2');
 insert into jobs(botId, img1, img2, img3, txt1, txt2)
+
 values (2,
         'https://s1.328888.xyz/2022/07/03/Ovfp.png',
         'https://s1.328888.xyz/2022/07/03/jN4F.png',
         '',
         '测试数据1', '测试数据2');
 
-# select uid
-# from users
-# where uname = 'aki';
-# select botId
-# from bot
-# where uid = 2;
+update jobs set complete=1 where complete=0;
+
+select * from jobs where uid=? and complete=0;
+
+update jobs set botId=? where jobs.id = 1;
+
+###################################《数据操作》#########################################
+# 级联查询
 select * from bot,jobs where bot.botId=jobs.botId ;
 select * from bot,jobs,user where bot.botId=jobs.botId and user.uid=bot.uid;
 
+# bot
+select botId,baseUrl,accessToken,secret from bot where uid = 1;
 
+insert into bot (uid, accessToken, secret)
+values (1,'','');
+
+update bot set secret='dasdsdasdad', accessToken='d49fd2eb77d0a96520a3ec6261b514f02cee6e61b06d9a49723338696683777e' where botId=6;
+
+delete from bot where bot.botId=9;
+
+# user
+select * from user where uid = 1;
+insert into user(uname, pwd) values ( ?, ?);
+update user set uname=?, pwd=? WHERE uid=1;
+delete from user where uid=1;
+select uid,pwd from user where uname = 'admin';
+
+#jobs
+select * from jobs where botId = 1;
+
+insert into jobs(botId, img1, img2, img3, txt1, txt2)
+values (?,?,?,?,?,?);
+update jobs set img1=?, img2=?,img3=?,txt1=?,txt2=? WHERE botId=1;
+delete from jobs where botId=1;
+
+select * from bot,jobs where bot.botId=jobs.botId and jobs.complete=0;
